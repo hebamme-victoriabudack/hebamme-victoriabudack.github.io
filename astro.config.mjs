65fc7c08d98fdaf37e73af49bd853936b7211e79
eric-mb <ericmb@tuta.io>
2026-01-08 20:57:26 +0100

    convert to cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,7 +13,8 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "https://hebamme-dresden.eu",
-  base: config.site.base_path ? config.site.base_path : "/",
+  // base: config.site.base_path ? config.site.base_path : "/",
+  output: "static",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
   // image: { service: sharp() },
   vite: { plugins: [tailwindcss()] },
